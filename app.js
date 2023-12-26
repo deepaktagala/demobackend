98c7052c91ec6808ecc1cb0e9bc91295a4802953
Deepak Kumar <deepaktagala0@gmail.com>
2023-12-26 13:30:55 +0530

    github data added

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+const githubData ={
+    "name": "Deepak",
+    "class": "MCA",
+    "project": "First BackEnd"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
@@ -16,7 +22,8 @@ app.get('/twitter',(req,res)=>{
 });
 app.get('/youtube',(req,res)=>{
     res.send('<a href="https://www.youtube.com/watch?v=pOV4EjUtl70&t=810s"> How to deploy backend code in production')
-})
+});
+app.get()
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
