3ace8127e0c96c52d31d7a0047ff99e926ea43bd
Deepak Kumar <deepaktagala0@gmail.com>
2023-12-26 12:46:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/login', (req,res)=>{
+    res.send('Login')
+});
+
+app.get('/twitter',(req,res)=>{
+    res.send('twitter deepak')
+});
+app.get('/youtube',(req,res)=>{
+    res.send('<a href="https://www.youtube.com/watch?v=pOV4EjUtl70&t=810s"> How to deploy backend code in production')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+});
